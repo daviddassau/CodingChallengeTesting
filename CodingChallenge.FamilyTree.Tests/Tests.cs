4fc848348aa1cd786de4c25d08bc9fe58bbf7ad3
David Dassau <todieahero@hotmail.com>
2018-04-21 10:47:13 -0500

    added files

---
+++ CodingChallenge.FamilyTree.Tests/Tests.cs
@@ -0,0 +1,34 @@
+﻿using Given.Common;
+using NUnit.Framework;
+
+namespace CodingChallenge.FamilyTree.Tests
+{
+    [TestFixture]
+    public class TreeTests
+    {
+        
+        [Test]
+        public void if_the_person_exists_in_tree_the_result_should_be_january()
+        {
+            var tree = FamilyTreeGenerator.Make();
+            var result = new Solution().GetBirthMonth(tree, "Joe");
+            result.ShouldEqual("January");
+        }
+           
+        [Test]
+        public void if__the_person_exists_at_the_top_tree_the_result_should_be_may()
+        {
+            var tree = FamilyTreeGenerator.Make();
+            var result = new Solution().GetBirthMonth(tree, "Ted");
+            result.ShouldEqual("May");
+        }
+
+        [Test]
+        public void if_the_person_does_not_exist_in_the_tree_the_result_should_be_empty()
+        {
+            var tree = FamilyTreeGenerator.Make();
+            var result = new Solution().GetBirthMonth(tree, "Jeebus");
+            result.ShouldEqual("");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# CodingChallengeTesting
+CodingChallenge
+===============
+
+code challenges for interviews
