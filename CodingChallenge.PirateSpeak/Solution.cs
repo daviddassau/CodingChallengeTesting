ed1b3d9b2a10a0c3224db1d62f04472202350677
David Dassau <todieahero@hotmail.com>
2018-04-21 13:06:07 -0500

    got PirateSpeak challenge to work

--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CodingChallenge.PirateSpeak
@@ -7,7 +8,39 @@ namespace CodingChallenge.PirateSpeak
     {
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            var result = new List<string>();
+            var jumbleSorted = new string(jumble.ToCharArray().OrderBy(x => x).ToArray());
+
+            foreach (var word in dictionary)
+            {
+                if (word.Length == jumble.Length)
+                {
+                    var wordSorted = new string(word.ToCharArray().OrderBy(x => x).ToArray());
+
+                    if (string.Equals(wordSorted,jumbleSorted,StringComparison.OrdinalIgnoreCase))
+                        result.Add(word);
+                }
+
+                //if (word.Length == jumble.Length)
+                //{
+                //    var wordCharacters = word.ToCharArray().ToList();
+                //    var numberOfMatches = 0;
+
+                //    foreach (var character in jumbleCharacters)
+                //    {
+                //        if (wordCharacters.Contains(character))
+                //        {
+                //            wordCharacters.Remove(character);
+                //            numberOfMatches++;
+                //        }
+                //    }
+
+                //    if (numberOfMatches == jumble.Length)
+                //        result.Add(word);
+                //}
+            }
+
+            return result.ToArray();
         }
     }
 }
